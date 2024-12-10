97c5c37c5837472729803b2fd37b021696cb4e26
Maksim <maksim0708@bk.ru>
2024-12-10 14:27:58 +0300

    Update dz_1.py

--- dz_1.py
+++ dz_1.py
@@ -1,4 +1,4 @@
-1)
+#1)
 import math
 r=int(input('Введите радиус окр в см '))
 dlin=2*math.pi*r
@@ -6,13 +6,13 @@ s=math.pi*r**2
 print(f'длинна окр {round(dlin,2)}')
 print(f'Площадь окр {round(s,2)}')
 
-2)
+#2)
 x,y=10,55
 print(f'x={x},y={y}')
 x,y=y,x
 print(f'x={x},y={y}')
 
-3)
+#3)
 import math
 l=int(input('Введите длину маятника '))
 g=9.81
